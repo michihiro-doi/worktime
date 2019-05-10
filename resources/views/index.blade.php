58f966d30b52565c191203a54435c4cfdb25df25
Michihiro　Doi <48142612+michi-ddd@users.noreply.github.com>
2019-05-10 13:28:55 +0900

    indexリンクボタン作成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,17 @@
         <div class="card row">
             <div class="card-header">勤怠アプリ</div>
             <div class="card-body">
+                <div class="row text-center">
+                    <div class="col-4">
+                        <a  href="/store" class="btn-lg btn-primary">スタッフ追加</a>
+                    </div>
+                    <div class="col-4">
+                        <a  href="/record" class="btn-lg btn-primary">勤怠管理</a>
+                    </div>
+                    <div class="col-4">
+                        <a  href="/data" class="btn-lg btn-primary">出勤記録</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
